7a7af56ec8bed5c1e796ffe4e130dc9f9551e50e
Jean Carlos <jeanjnx@gmail.com>
2020-12-24 17:51:58 -0300

    Configuring Eureka clients

--- hr-worker/src/main/java/com/jeanjn/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/jeanjn/hrworker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package com.jeanjn.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 

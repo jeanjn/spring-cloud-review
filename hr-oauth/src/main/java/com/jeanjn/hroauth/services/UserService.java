13b483f86fa37020b05a27a4165f72a3fb10e39a
Jean Carlos <jeanjnx@gmail.com>
2020-12-26 18:13:31 -0300

    Login, Jwt token

--- hr-oauth/src/main/java/com/jeanjn/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/jeanjn/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.jeanjn.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.jeanjn.hroauth.entities.User;
 import com.jeanjn.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,18 @@ public class UserService {
 		}
 		
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("username not found: " + username);
+			throw new UsernameNotFoundException("username not found");
+		}
+		
+		logger.info("username found: " + username);
 		return user;
 	}
 }
